10f6b011bd7874bb2513c4afa27c5496869f0cdf
Gwynie Dunlevy <78832427+gdunlevy@users.noreply.github.com>
2022-04-18 21:38:01 -0400

    Update posDetection.py

--- posDetection.py
+++ posDetection.py
@@ -1,57 +1,74 @@
-import os 
+import os , string
 import pandas as pd
 import spacy
 from PyDictionary import PyDictionary
 from pprint import pprint
+from nltk.corpus import stopwords 
+from nltk.stem.wordnet import WordNetLemmatizer
+from gensim.models import KeyedVectors,Word2Vec
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 sp = spacy.load('en_core_web_sm')
+stop = set(stopwords.words('english'))
+exclude = set(string.punctuation)
+lemma = WordNetLemmatizer() 
+
+
 
 def read(f): 
 	df = pd.read_csv(f)
-	words = []
 
-	for index, row in df.iterrows(): 
-		for col in df.columns: 
-			column = col
-		words.append(str(row[column]))
+	sentence = df.iloc[:, 1].tolist()
+	for i in range(len(sentence)): 
+		sentence[i] = sentence[i].lower()
 
 
-	sentence = ' '.join(str(w) for w in words)
 
-	return (sentence)
+	sentence = " ".join(sentence)
 
+
+	stop_free = " ".join([i for i in sentence.lower().split() if i not in stop])
+
+	# remove any stop words present
+	punc_free = ''.join(ch for ch in stop_free if ch not in exclude)  
+
+	# remove punctuations + normalize the text
+	normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split()) 
+	print(normalized)
+
+	return (normalized)
 
 def pos(sentence): 
 
 	dictionary=PyDictionary()
 	sen = sp(sentence)
-	for word in sen:
-		print(word)
 
-		print(f'{word.text:{12}} {word.pos_:{10}} {word.tag_:{8}} {spacy.explain(word.tag_)}')
+	sub_toks = [tok for tok in sen if (tok.dep_ == "nsubj") ]
+
+	sen2 = sp(str(sub_toks[0]))
+	print(sen2)
+
+
+	for i in sen:
+		print("Similarity:", sen2.similarity(i))
 		
-
+	'''
+	'I used to be a banker but I lost interest --> used banker lost interest 
+		banker is the subject 
+	check similarity of other words against the subject
+		highest similarity is INTEREST (0.41) 
 		
-		#if word.pos_ == 'DET' or word.pos_ == 'CCONJ' or word.pos_ == 'PUNCT' or word.pos_ =='PART': 
-		definition = dictionary.meaning(str(word))
-		print(definition)
-
-		'''
-		if word.pos_ == 'PRON' or word.pos_ == 'NOUN'or word.pos_ == 'PROPN' and keys == 'Noun':
-			pprint(definition['Noun'])
-		exit()
-		'''
-
+	
+	'When the church nought gas for the barbecue, proceeds went from sacred to the propane. --> church bought gas annual barbecue proceeds went sacred propan
+		church is the subject 
+	check similarity of other words against the subject
+		highest similarity is PROPANE (0.41) 
+		
+		
+	I am not sure what to do about the puns with quotes.... 
 	'''
-	can split up definitions based off of the PoS tag given 
-	some will have to be based off the tags (when it is an AUX)
-		VB --> verb 
-	can ignore DET and CCONJ and PUNCT
 
-
-	check for multiple definitions of 
-
-	'''
 
 def main():
     # parse xml file
@@ -62,9 +79,11 @@ def main():
 	    if os.path.isfile(f):
 	    	if f.endswith((".csv")):
 	    		#name = os.path.splitext(filename)[0]
+	    		print(f)
 	    		sentence = read(f)
 	    		pos(sentence)
-	    		exit()
+
+	    		#exit()
 
 
 
